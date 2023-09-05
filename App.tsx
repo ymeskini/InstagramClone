1989b1d648b076bd18e3acdd77e47229f925eee7
Youssef Meskini <youss.meskini@gmail.com>
2023-09-05 11:40:35 +0100

    add post view

--- App.tsx
+++ App.tsx
@@ -1,112 +1,141 @@
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import {Image, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import Entypo from 'react-native-vector-icons/Entypo';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Feather from 'react-native-vector-icons/Feather';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Icon name="etsy" />
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import fonts from './src/theme/fonts';
+import colors from './src/theme/colors';
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView>
+      <View style={styles.post}>
+        <View style={styles.header}>
+          <Image
+            style={styles.avatar}
+            source={{
+              uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg',
+            }}
+          />
+          <Text style={styles.username}>ymeskini</Text>
+          <Entypo
+            name="dots-three-horizontal"
+            size={16}
+            style={styles.threeDots}
+          />
         </View>
-      </ScrollView>
+        <Image
+          style={styles.image}
+          source={{
+            uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg',
+          }}
+        />
+
+        <View style={styles.footer}>
+          <View style={styles.iconContainer}>
+            <AntDesign
+              name={'hearto'}
+              size={24}
+              style={styles.icon}
+              color={colors.black}
+            />
+            <Ionicons
+              name="chatbubble-outline"
+              size={24}
+              style={styles.icon}
+              color={colors.black}
+            />
+            <Feather
+              name="send"
+              size={24}
+              style={styles.icon}
+              color={colors.black}
+            />
+            <Feather
+              name="bookmark"
+              size={24}
+              style={styles.bookmark}
+              color={colors.black}
+            />
+          </View>
+
+          <Text style={styles.text}>
+            Liked by <Text style={styles.bold}>lgrinevic</Text> and{' '}
+            <Text style={styles.bold}>45,678 others</Text>
+          </Text>
+
+          <Text style={styles.text}>
+            <Text style={styles.bold}>ymeskini</Text> Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Nam tempore alias eos doloribus
+            consequatur delectus iste debitis placeat, dolorum unde distinctio
+            doloremque velit ducimus, blanditiis quibusdam. Provident,
+            repudiandae minus. Cum.
+          </Text>
+
+          <Text>View all 16 comments</Text>
+          <View style={styles.comment}>
+            <Text style={styles.commentText}>
+              <Text style={styles.bold}>ymeskini</Text> Lorem ipsum dolor sit
+              amet consectetur adipisicing elit.
+            </Text>
+            <AntDesign name="hearto" style={styles.icon} color={colors.black} />
+          </View>
+          <Text>19 August, 2023</Text>
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  post: {},
+  image: {
+    width: '100%',
+    aspectRatio: 1,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  header: {
+    flexDirection: 'row',
+    padding: 10,
+    alignItems: 'center',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  avatar: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginRight: 10,
   },
-  highlight: {
-    fontWeight: '700',
+  username: {
+    fontWeight: fonts.weight.bold,
+    color: colors.black,
+  },
+  threeDots: {
+    marginLeft: 'auto',
+  },
+  iconContainer: {
+    flexDirection: 'row',
+  },
+  icon: {
+    marginHorizontal: 5,
+  },
+  bookmark: {marginLeft: 'auto'},
+  footer: {
+    padding: 10,
+  },
+  bold: {fontWeight: fonts.weight.bold},
+  text: {
+    color: colors.black,
+    lineHeight: 18,
+  },
+  comment: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  commentText: {
+    color: colors.black,
+    flex: 1,
+    lineHeight: 18,
   },
 });
 
