7c0b94394dd56a019ef4e2e3b6b00b731f7b5d1d
Youssef Meskini <youss.meskini@gmail.com>
2023-11-28 11:01:00 +0100

    add first stack navigator

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,12 +1,54 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import {Image, View} from 'react-native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
 import {HomeScreen} from '../screens/HomeScreen';
 import {ProfileScreen} from '../screens/ProfileScreen';
+// @ts-ignore
+import logo from '../assets/logo.png';
+
+const Stack = createNativeStackNavigator();
 
 export const Navigation = () => {
   return (
     <NavigationContainer>
-      <HomeScreen />
+      <Stack.Navigator
+        initialRouteName="home"
+        screenOptions={{
+          headerShown: true,
+        }}>
+        <Stack.Screen
+          options={{
+            headerTitle: Header,
+            headerTitleAlign: 'center',
+          }}
+          component={HomeScreen}
+          name="home"
+        />
+        <Stack.Screen
+          component={ProfileScreen}
+          name="profile"
+          options={{
+            title: 'Profile',
+          }}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
+  );
+};
+
+const Header = () => {
+  return (
+    <View>
+      <Image
+        source={logo}
+        resizeMode="contain"
+        style={{
+          width: 150,
+          height: 45,
+        }}
+      />
+    </View>
   );
 };
